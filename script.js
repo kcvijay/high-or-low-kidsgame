d7da5e49096726d61a37041594758636dc35fb64
kcvijay <vj.kc2007@gmail.com>
2022-07-10 08:55:28 +0300

    Third Commit - play audio

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const mainContainer = document.querySelector(".main-container");
+
+const reload = document.querySelector(".reload");
+
 const imgBtnUp = document.querySelector(".img-btn-up");
 const imgBtnDown = document.querySelector(".img-btn-down");
 const imgBtnEqual = document.querySelector(".img-btn-equal");
@@ -6,6 +9,7 @@ const guessOne = document.querySelector(".guess-box-one");
 const guessTwo = document.querySelector(".guess-box-two");
 
 const gameOver = document.querySelector(".game-over");
+const gameOverHeading = document.querySelector(".game-over-heading");
 const playAgain = document.querySelector(".play-again");
 const lastScore = document.querySelector(".last-score");
 
@@ -22,38 +26,40 @@ window.addEventListener("load", () => {
   countdownTimer();
 });
 
+//** Refreshing a game on reload button click */
+reload.addEventListener("click", () => {
+  location.reload();
+});
+
 //On up/down/equal button click, generate random numbers on both number boxes.
 imgBtnUp.addEventListener("click", () => {
   if (guessTwo.innerHTML > guessOne.innerHTML) {
     scoreAdd();
+    guessOne.innerHTML = getRandom(101);
+    guessTwo.innerHTML = getRandom(101);
   } else {
     failInit();
   }
-
-  guessOne.innerHTML = getRandom(101);
-  guessTwo.innerHTML = getRandom(101);
 });
 
 imgBtnDown.addEventListener("click", () => {
   if (guessTwo.innerHTML < guessOne.innerHTML) {
     scoreAdd();
+    guessOne.innerHTML = getRandom(101);
+    guessTwo.innerHTML = getRandom(101);
   } else {
     failInit();
   }
-
-  guessOne.innerHTML = getRandom(101);
-  guessTwo.innerHTML = getRandom(101);
 });
 
 imgBtnEqual.addEventListener("click", () => {
   if (guessTwo.innerHTML === guessOne.innerHTML) {
     scoreAdd();
+    guessOne.innerHTML = getRandom(101);
+    guessTwo.innerHTML = getRandom(101);
   } else {
     failInit();
   }
-
-  guessOne.innerHTML = getRandom(101);
-  guessTwo.innerHTML = getRandom(101);
 });
 
 playAgain.addEventListener("click", () => {
@@ -67,8 +73,25 @@ playAgain.addEventListener("click", () => {
 function getRandom(max) {
   return Math.floor(Math.random() * max);
 }
+getRandom();
+
+function countdownTimer() {
+  let timeleft = 59;
+  const countdown = setInterval(function () {
+    if (timeleft <= 0) {
+      clearInterval(countdown);
+      failInit();
+      audioWrong.play();
+      gameOverHeading.innerHTML = "OOOPS! Time is up.";
+    } else {
+      timerTxt.innerHTML = `Remaining time: ${timeleft} seconds`;
+    }
+    timeleft -= 1;
+  }, 1000);
+}
 
 function failInit() {
+  audioWrong.play();
   scoreTxt.innerHTML = 0;
   lastScore.innerHTML = score;
   gameOver.style.display = "flex";
@@ -80,22 +103,10 @@ function scoreAdd() {
   scoreTxt.innerHTML = score;
 }
 
-//Timer function
-function countdownTimer() {
-  let timeleft = 60;
-  const countdown = setInterval(function () {
-    if (timeleft <= 0) {
-      clearInterval(countdown);
-      timerTxt.innerHTML = "Time is up";
-    } else {
-      remainingTime.innerHTML = timeleft;
-    }
-    timeleft -= 1;
-  }, 1000);
-}
+//play audio on wrong answer/timesup
+let audioWrong = new Audio("/media/wrong-sound.wav");
 
-/*function startInit() {
-  gameOver.style.display = "none";
-  scoreTxt.innerHTML = 0;
-  mainContainer.style.display = "block";
-}*/
+console.log(
+  `This is a simple High or Low game especially designed for kids learning the numbers.
+  Instuction is simple: player presses either high, low or equal button for the number below in compare to the number above. For example, if the number above is 50 and below is 60, player presses "High" button. Time limit is 60 seconds.`
+);
